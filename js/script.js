118a138a681c60ecf8c2ff360adf5d23a8530520
mod26-k <kellirinaw@gmail.com>
2023-03-08 14:25:28 -0500

    updates

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-// <!-- Alphabet -->
+// Keyboard options
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+// Displays keyboard
 const alphabetButtons = document.getElementById('letters');
 function displayLetterButtons() {
     for (let i = 0; i < alphabet.length; i++) {
@@ -10,85 +11,85 @@ function displayLetterButtons() {
         alphabetButtons.appendChild(newButton)
     }
 }
-    
-
-
-    function storeUserGuesses() {
-        const guessedLetter = document.getElementById(alphabetButtons.innerText)
-        console.log(guessedLetter)
-}
-    alphabetButtons.addEventListener('click', storeUserGuesses())
 displayLetterButtons()
 
+// Stores keyboard selection into array, and pushes array data into ul and disables selected letter
+const selectedLetters = storeUserGuesses.clickedButton
 const userGuesses = []
+function storeUserGuesses(event) {
+    const clickedButton = event.target;
+    const guessedLetter = event.target.id;
+        clickedButton.disabled = true
+        userGuesses.push(guessedLetter)
+            console.log(userGuesses)
+}
 
-// function storeUserGuesses() {
-//     const letter = document.getElementById(alphabet[i])
-//     console.log(letter)
-// }
+
+// Counts and displays amount of turns left
+let numOfClicks = 6
+const counterDisplay = document.getElementById
+('counter')
+counterDisplay.innerText = ('You have ' + numOfClicks + ' lives left')
+
+function calculateGuessesLeft(event) {
+    numOfClicks -= 1;
+    counterDisplay.innerText = ('You have ' + numOfClicks + ' lives left')
+    const keyboard = document.querySelector('fieldset')
+    if (numOfClicks === 0)
+        return keyboard.disabled = true;
+}
+
+alphabetButtons.addEventListener('click', storeUserGuesses)
+alphabetButtons.addEventListener('click', calculateGuessesLeft)
 
 // CATEGORIES
 const categories = ['countries']
 
-// <!-- Lives -->
-// - will need a counter that provides 6 "lives" 
-// - counter goes down by 1 with each "click" on a letter, if letter is not in generated string
-// - look into how to subtract/ opposite of i++? i--?
-// - does i become > instead of < when counting backwards?
-// can only guess a letter once
-
-// TIMER 10 MIN
-
-
-// function liveCount {
-//     // (display6Lives) {
-// //  (eventListenerOnAlphabet) {
-// //         for (i = 6; i > alphabet.length; i--)
-//     // }
-// }
-
-
-// <!-- Guess Word -->
-// - make randomWord function work for any/all category arrays (in case there are more-- will function like a method)
-
+// New game is started when a category is selected, and a random string from the category is generated
 const countries = ['Algeria', 'Albania', 'Botswana', 'Bulgaria', 'Cambodia', 'Cameroon', 'Djibouti', 'Denmark', 'Eritrea', 'Estonia', 'Finland', 'Fiji', 'Guatemala', 'Grenada', 'Haiti', 'Honduras', 'Israel', 'Indonesia', 'Jordan', 'Jamaica', 'Kenya', 'Kiribati', 'Lebanon', 'Liechtenstein', 'Malawi', 'Moldova', 'Nicaragua', 'Nauru'];
 
-function newGame(word) {
-    const randomIdx = Math.floor(Math.random() * countries.length)
-    const generatedWord = countries[randomIdx].toLowerCase()
-    const splitWord = generatedWord.split('')
-        // generatedWord.push(countriesCat[randomIdx])
+// Array where the generated word will be stored
+let genWordArr = []
+
+// List where generated word will be displayed
+const charList = document.getElementById('list');
+
+// Creates a new game
+const randomIdx = Math.floor(Math.random() * countries.length)
+const generatedWord = countries[randomIdx].toLowerCase()
+const splitWord = generatedWord.split('')
+
+function newGame() {
+    // Generates new word, converts it to lowercase and splits each letter
+    // const randomIdx = Math.floor(Math.random() * countries.length)
+    // const generatedWord = countries[randomIdx].toLowerCase()
+    // const splitWord = generatedWord.split('')
+        // const noCommas = splitWord.join(',',' ')
+        // genWordArr.push(splitWord)
         console.log(splitWord)
+
+    // Creates a new list item for each letter of the generated word
+        splitWord.forEach((letter) => {
+            let character = document.createElement('li');
+            character.innerText = letter;
+            charList.appendChild(character);
+            charList.style.visibility = "hidden";
+        })
+
+    // 
+
+
+    // Re-enables the keyboard
+    const keyboard = document.querySelector('fieldset')
+    keyboard.disabled = false;
+    // storeUserGuesses.clickedButton.disabled = false;
+    // const clickedButton = event.target;
+        // clickedButton.disabled = false;
+    //reset guess counter to 6
+    // counterDisplay.innerText = ('You have ' + numOfClicks + ' lives left')
+        // calculateGuessesLeft()
+    // let genWordArr = []
 }
-
-// Start Button
-
-// const start = document.getElementById('start')
-// start.addEventListener('click', newGame)
-
-//Array of logged word and array of guessed letters
-
-
-// Declaration of win/lose
-
-
-// function winLose() {
-// // if (counter >= 0 && input === randomWord) {
-// //     return document.getElementById('win');
-// // else {
-// //     return lose = 'You Lose!';
-// // }
-// }
-
-// <!-- Ideally, if time permits -->
-// - add different categories with larger library of possible words
-// - add a "hint" button that generates hint depending on the generated word
-// - allow for 2 players;
-
-// categories.push('new category')
-// console.log(categories)
-
-// 
 
 // NEW CATEGORIES GO HERE:
 
@@ -104,3 +105,19 @@ function displayCategoryButtons() {
     categoryButtons.addEventListener('click', newGame)
 }
 displayCategoryButtons()
+
+
+// Declaration of win/lose
+
+// function winLose() {
+// // if (counter >= 0 && input === randomWord) {
+// //     return document.getElementById('win');
+// // else {
+// //     return lose = 'You Lose!';
+// // }
+// }
+
+// <!-- Ideally, if time permits -->
+// - add different categories with larger library of possible words
+// - add a "hint" button that generates hint depending on the generated word
+// - allow for 2 players;
